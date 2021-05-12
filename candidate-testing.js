3f5fb6627ebbb534ff24b6f72978747827931ba2
Michael Williams <>
2021-05-12 19:23:45 +0000

    corrected spaces and question numbering

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,7 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
-  "1) Who was the first American woman in space?", "2) True or false: 5000 meters = 5 kilometers.", "3) (5 + 3)/2 * 10 = ?", "4) Given the array [8, \"Orbit\", \"Trajectory\", 45], what entry is at index 2?", "5) What is the minimum crew size for the ISS?"
+  "Who was the first American woman in space?", "True or false: 5000 meter = 5 kilometer. ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, \"Orbit\", \"Trajectory\", 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "
   ];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
@@ -38,7 +38,7 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   for (i = 0; i < questions.length; i++){
-    console.log(questions[i]);
+    console.log((i +1) + ")",questions[i]);
     candidateAnswers[i] = input.question("Your Answer: ");
     console.log("Correct Answer:", correctAnswers[i]);
     console.log();
